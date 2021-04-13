0b4b2f5c43b0fb388915db2abc36b3c51510fe2e
Felicia <shuyazhao2016@gmail.com>
2021-04-13 19:28:45 -0400

    Statistics on snli dataset

--- dataset_0412.py
+++ dataset_0412.py
@@ -10,101 +10,101 @@ from transformers import BertTokenizer
 
 
 class NLIDataset(Dataset):
-	def __init__(self,root='/media/felicia/Data/{data}', data="multinli",split="train",tokenized=True,padding=False,max_length=12):
-		super(NLIDataset,self).__init__()
-		self.root=root.format(data=data)
-		self.tokenized=tokenized
-		self.padding=padding
-		self.max_length=max_length
-		self.jsonfile="{}_1.0_{}.jsonl".format(data,split) # change format if needed
-		self.filename=os.path.join(self.root,self.jsonfile)
+    def __init__(self, root='/media/felicia/Data/{data}', data="multinli", split="train", tokenized=True,padding=True, truncation=True, max_length=12):
+        super(NLIDataset, self).__init__()
+        self.root = root.format(data=data)
+        self.tokenized = tokenized
+        self.padding = padding
+        self.truncation = truncation
+        self.max_length = max_length
+        self.jsonfile = "{}_1.0_{}.jsonl".format(data, split)  # change format if needed
+        self.filename = os.path.join(self.root, self.jsonfile)
 
-		self.num_labels = 2
-		self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-		self.LABEL_MAP = {
-                "entailment": 0,
-                "neutral": 1,
-                "contradiction": 2,
-                "hidden": 0
-            }
+        self.num_labels = 2
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',
+                        padding=self.padding, truncation=self.truncation,max_length=self.max_length)
+        self.LABEL_MAP = {
+            "entailment": 0,
+            "neutral": 1,
+            "contradiction": 2,
+            "hidden": 0
+        }
 
-		self.data=[]
-		self.sentences={"sent1":[],"sent2":[]}
-		self.labels=[]
+        self.data = []
+        self.sentences = {"sent1": [], "sent2": []}
+        self.labels = []
 
-		self.load_data()
+        self.load_data()
 
-	def load_data(self):
-		with open(self.filename) as f:
-			for line in f:
-				example=json.loads(line) # dict
-				self.data.append(example)
-		self.parseAll()
+    def load_data(self):
+        with open(self.filename) as f:
+            for line in f:
+                example = json.loads(line)  # dict
+                self.data.append(example)
+        self.parseAll()
 
+    def tokenizeSent(self, sentence):
 
-	def tokenizeSent(self,sentence):
+        tokenized_text = self.tokenizer.tokenize(sentence)
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
 
-		tokenized_text = self.tokenizer.tokenize(sentence)
-		indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+        # if self.padding and len(indexed_tokens) < self.max_length:
+        #     indexed_tokens += [0] * (self.max_length - len(indexed_tokens))
+        # else:
+        #     indexed_tokens = indexed_tokens[:self.max_length]
+        indexed_tokens = np.array(indexed_tokens)
 
-		if self.padding and len(indexed_tokens) < self.max_length:
-			indexed_tokens += [0] * (self.max_length - len(indexed_tokens))
-		else:
-			indexed_tokens = indexed_tokens[:self.max_length]
-		indexed_tokens = np.array(indexed_tokens)
+        return indexed_tokens
 
-		return indexed_tokens
+    def parseAll(self):
+        for i, text in tqdm(enumerate(self.data)):
+            sent1 = text["sentence1"]
+            sent2 = text["sentence2"]
+            label = text["gold_label"]
+            if label not in self.LABEL_MAP:
+                continue
 
+            if self.tokenized:
+                sent1 = self.tokenizeSent(sent1)
+                sent2 = self.tokenizeSent(sent2)
 
-	def parseAll(self):
-		for i, text in tqdm(enumerate(self.data)):
-			sent1=text["sentence1"]
-			sent2=text["sentence2"]
-			label=text["gold_label"]
-			if label not in self.LABEL_MAP:
-				continue
+            self.sentences["sent1"].append(sent1)
+            self.sentences["sent2"].append(sent2)
+            self.labels.append(self.LABEL_MAP[label])
 
-			if self.tokenized:
-				sent1=self.tokenizeSent(sent1)
-				sent2=self.tokenizeSent(sent2)
+    def __getitem__(self, index):
+        sent1 = self.sentences["sent1"][index]
+        sent2 = self.sentences["sent2"][index]
+        label = self.labels[index]
 
-			self.sentences["sent1"].append(sent1)
-			self.sentences["sent2"].append(sent2)
-			self.labels.append(self.LABEL_MAP[label])
-    
-	def __getitem__(self,index):
-		sent1 = self.sentences["sent1"][index]
-		sent2 = self.sentences["sent2"][index]
-		label = self.labels[index]
+        return index, sent1, sent2, label
 
-		return index,sent1,sent2,label
-
-	def __len__(self):
-		return len(self.data)
+    def __len__(self):
+        return len(self.labels)
 
 
 if __name__ == "__main__":
-	"""
-	data="multinli"/"snli"  
-	split=
-		"train","dev_matched","dev_mismatched"
-		"train","test","dev" fo "snli"
-	tokenized=True(default) or False
-	padding= True or False(default)
-	max_length=12 (default)
-	"""
+    """
+    data="multinli"/"snli"  
+    split=
+        "train","dev_matched","dev_mismatched"
+        "train","test","dev" fo "snli"
+    tokenized=True(default) or False
+    padding= True or False(default)
+    max_length=12 (default)
+    """
 
-	sentset=NLIDataset(data='snli',split="test",tokenized=False,padding=False)
-
-	BATCH=1
-	contentloader=torch.utils.data.DataLoader(sentset,batch_size=BATCH,shuffle=True,num_workers=4)
-
-	for idx,data in enumerate(contentloader):
-		index,sent1,sent2, label=data
-		print(index) # tensor: b
-		print(sent1) # tensor:  b * max_length
-		print(sent2) # tensor:  b * max_length
-		print(label) # tensor: b
-
-		if idx>10:
-			break
+    # sentset = NLIDataset(data='snli', split="test", tokenized=False, padding=False)
+    #
+    # BATCH = 1
+    # contentloader = torch.utils.data.DataLoader(sentset, batch_size=BATCH, shuffle=True, num_workers=4)
+    #
+    # for idx, data in enumerate(contentloader):
+    #     index, sent1, sent2, label = data
+    #     print(index)  # tensor: b
+    #     print(sent1)  # tensor:  b * max_length
+    #     print(sent2)  # tensor:  b * max_length
+    #     print(label)  # tensor: b
+    #
+    #     if idx > 10:
+    #         break
---
+++ preprocess.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizer
+
+import matplotlib.pyplot as plt
+
+from dataset_0412 import NLIDataset
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset_type', type=str, default="snli")
+parser.add_argument('--dataset_split', type=str,default="train")
+
+args = parser.parse_args()
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+sentset = NLIDataset(data=args.dataset_type, split=args.dataset_split, tokenized=True,padding=False, truncation=False)
+
+BATCH = 1
+contentloader = torch.utils.data.DataLoader(sentset, batch_size=BATCH, shuffle=False, num_workers=4)
+
+sent1_list=[]
+sent2_list=[]
+label_list=[]
+
+for idx, data in tqdm(enumerate(contentloader)):
+    _, sent1, sent2, label = data
+    sent1_list.append(sent1.numpy()[0])
+    sent2_list.append(sent2.numpy()[0])
+    label_list.append(label.numpy())
+
+sent1_length=[len(x) for x in sent1_list]
+sent2_length=[len(x) for x in sent2_list]
+
+
+plt.grid()
+plt.hist(sent1_length,30,color='xkcd:turquoise', edgecolor='tab:gray')
+plt.xlabel('Length of Sentence',fontsize=16)
+plt.ylabel('Number of Users',fontsize=16)
+plt.title('length of Sentence1 in {} {} Dataset'.format(args.dataset_type,args.dataset_split),fontsize=16,fontweight='bold')
+plt.xticks()
+plt.yticks()
+# plt.show()
+plt.savefig("vis/{}_{}_sentence1_hist.png".format(args.dataset_type,args.dataset_split))
+plt.close()
+
+plt.grid()
+plt.hist(sent2_length,30,color='xkcd:turquoise', edgecolor='tab:gray')
+plt.xlabel('Length of Sentence',fontsize=16)
+plt.ylabel('Number of Users',fontsize=16)
+plt.title('Length of Sentence2 in {} {} Dataset'.format(args.dataset_type,args.dataset_split),fontsize=16,fontweight='bold')
+plt.xticks()
+plt.yticks()
+# plt.show()
+plt.savefig("vis/{}_{}_sentence2_hist.png".format(args.dataset_type,args.dataset_split))
+plt.close()
+
+
+"""
+ipython preprocess.py --dataset_split=train
+"""
