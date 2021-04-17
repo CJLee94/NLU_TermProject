420790f4236c4c031b5c285257b63006a6c72c26
Felicia <shuyazhao2016@gmail.com>
2021-04-17 04:25:18 -0400

    Update dataset for Albert/Roberta and MultiNLI/SNLI

--- dataset_0413.py
+++ dataset_0413.py
@@ -6,35 +6,40 @@ from tqdm import tqdm
 from collections import defaultdict
 
 import torch
-from torch.utils.data import Dataset, DataLoader,TensorDataset
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 
-from transformers import BertTokenizer,BertForSequenceClassification
+from transformers import BertTokenizer, RobertaTokenizer, T5Tokenizer, AlbertTokenizer
+from transformers import BertForSequenceClassification, AlbertForSequenceClassification, \
+    RobertaForSequenceClassification
 
 
 class NLIDataset(Dataset):
-    def __init__(self, root='/media/felicia/Data/{data}', data="multinli", split="train", padding=True, truncation=True, max_length=217):
+    def __init__(self, root='/media/felicia/Data/{data}', data="multinli", split="train", type="Roberta",
+                 tokenizer=None,max_length=217):
         super(NLIDataset, self).__init__()
         self.root = root.format(data=data)
-        self.padding = padding
-        self.truncation = truncation
-        self.max_length = max_length
         self.jsonfile = "{}_1.0_{}.jsonl".format(data, split)  # change format if needed
         self.filename = os.path.join(self.root, self.jsonfile)
+        self.max_length = max_length
 
-        self.num_labels = 2
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case = True)
+        self.num_labels = 3
         self.LABEL_MAP = {
             "entailment": 0,
             "neutral": 1,
             "contradiction": 2,
             # "hidden": 0
         }
+        self.type = type
+        if tokenizer is None:
+            self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", max_length=self.max_length)
+        else:
+            self.tokenizer = tokenizer
 
         self.data = []
-        self.sentences_a =[]
-        self.sentences_b =[]
+        self.sentences_a = []
+        self.sentences_b = []
         self.labels = []
-        self.features=defaultdict(list)
+        self.features = defaultdict(list)
 
         self.load_data()
 
@@ -57,17 +62,20 @@ class NLIDataset(Dataset):
             self.sentences_b.append(sent2)
             self.labels.append(self.LABEL_MAP[label])
 
-        self.convertToFeatures()
+        if self.type == "albert":
+            self.albertConvertToFeatures()
+        elif self.type == "roberta":
+            self.robertaConvertToFeatures()
 
-    def convertToFeatures(self):
+    def albertConvertToFeatures(self):
         for idx in tqdm(range(len(self.labels))):
-            tokens_a = self.tokenizer.tokenize(self.sentences_a[idx])
-            tokens_b = self.tokenizer.tokenize(self.sentences_b[idx])
+            tokens_a = self.tokenizer.tokenize(self.sentences_a[idx])[:-1] # ?
+            tokens_b = self.tokenizer.tokenize(self.sentences_b[idx])[:-1]
 
             self._truncate_seq_pair(tokens_a, tokens_b, self.max_length - 3)
 
             tokens = ["[CLS]"] + tokens_a + ["[SEP]"] + tokens_b + ["[SEP]"]
-            segment_ids = [0] * (len(tokens_a)+2)+[1]* (len(tokens_b)+1)
+            segment_ids = [0] * (len(tokens_a) + 2) + [1] * (len(tokens_b) + 1)
 
             input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
 
@@ -90,8 +98,39 @@ class NLIDataset(Dataset):
             self.features["segment_ids"].append(segment_ids)
             self.features["label_id"].append(self.labels[idx])
 
+    def robertaConvertToFeatures(self):
+        for idx in tqdm(range(len(self.labels))):
+            tokens_a = self.tokenizer.tokenize(self.sentences_a[idx])
+            tokens_b = self.tokenizer.tokenize(self.sentences_b[idx])
 
-    def _truncate_seq_pair(self,tokens_a, tokens_b,max_length):
+            self._truncate_seq_pair(tokens_a, tokens_b, self.max_length - 3)
+
+            tokens = ["<s>"] + tokens_a + ["</s>"] + tokens_b + ["</s>"]
+            segment_ids = [0] * (len(tokens_a) + 2) + [1] * (len(tokens_b) + 1)
+
+            input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+            # input_ids=tokens
+
+            # The mask has 1 for real tokens and 0 for padding tokens. Only real
+            # tokens are attended to.
+            input_mask = [1] * len(input_ids)
+
+            # Zero-pad up to the sequence length.
+            padding = [0] * (self.max_length - len(input_ids))
+            input_ids += padding
+            input_mask += padding
+            segment_ids += padding
+
+            assert len(input_ids) == self.max_length
+            assert len(input_mask) == self.max_length
+            assert len(segment_ids) == self.max_length
+
+            self.features["input_ids"].append(input_ids)
+            self.features["input_mask"].append(input_mask)
+            self.features["segment_ids"].append(segment_ids)
+            self.features["label_id"].append(self.labels[idx])
+
+    def _truncate_seq_pair(self, tokens_a, tokens_b, max_length):
         """Truncates a sequence pair in place to the maximum length."""
 
         # This is a simple heuristic which will always truncate the longer sequence
@@ -108,10 +147,10 @@ class NLIDataset(Dataset):
                 tokens_b.pop()
 
     def __getitem__(self, index):
-        input_ids=self.features["input_ids"][index]
-        input_mask=self.features["input_mask"][index]
-        segment_ids=self.features["segment_ids"][index]
-        label_ids=self.features["label_id"][index]
+        input_ids = self.features["input_ids"][index]
+        input_mask = self.features["input_mask"][index]
+        segment_ids = self.features["segment_ids"][index]
+        label_ids = self.features["label_id"][index]
 
         return torch.tensor(input_ids), torch.tensor(input_mask), torch.tensor(segment_ids), torch.tensor(label_ids)
 
@@ -132,28 +171,67 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     n_gpu = torch.cuda.device_count()
+    print("Number of GPU:",n_gpu)
 
-    BATCH = 5
+    BATCH = 4
 
     print("Data loading ...")
 
-    train_data = NLIDataset(data="multinli", split="train",  padding=False)
-    train_dataloader = DataLoader(train_data, batch_size=BATCH, shuffle=True)
+    ## tokenizer
+    # BTokenizer=BertTokenizer.from_pretrained("bert-base-uncased")
+    # ATokenizer=AlbertTokenizer.from_pretrained("albert-base-v1")
+    RTokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
-    # model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3)
-    # model.to(device)
-    # if n_gpu > 1:
-    #     model = torch.nn.DataParallel(model)
+    # tokenid=BTokenizer.convert_tokens_to_ids("[CLS]") # 101
+    # tokenid=ATokenizer.convert_tokens_to_ids("[CLS]") # 2
+    # tokenid=RTokenizer.convert_tokens_to_ids("[CLS]") # 3
+    #
+    # tokenid=BTokenizer.convert_tokens_to_ids("[SEP]") # 102
+    # tokenid=ATokenizer.convert_tokens_to_ids("[SEP]") # 3
+    # tokenid=RTokenizer.convert_tokens_to_ids("[SEP]") # 3
 
+    # albert_data = NLIDataset(type="albert",tokenizer=ATokenizer,data="multinli", split="dev_matched", max_length=10)
+    roberta_data = NLIDataset(type="roberta", tokenizer=RTokenizer, data="snli", split="test", max_length=10)
 
-    for idx, batch in tqdm(enumerate(train_dataloader)):
+    # albert_dataloader = DataLoader(albert_data, batch_size=BATCH, shuffle=True)
+    roberta_dataloader = DataLoader(roberta_data, batch_size=BATCH, shuffle=True)
+
+    ## model
+    # model=BertForSequenceClassification.from_pretrained("bert-base-uncased")
+    # model = AlbertForSequenceClassification.from_pretrained("albert-base-v1",num_labels=3 ,return_dict=False)
+    model = RobertaForSequenceClassification.from_pretrained("roberta-base",num_labels=3,return_dict=False)
+
+    model.to(device)
+    if n_gpu > 1:
+        model = torch.nn.DataParallel(model)
+
+    # print("Transfomer: Albert")
+    print("Transfomer: RoBERTa")
+
+    for idx, batch in tqdm(enumerate(roberta_dataloader)):
         batch = tuple(t.to(device) for t in batch)
         input_ids, input_mask, segment_ids, label_ids = batch
 
-        print(input_ids.size())  # tensor: b * max_length
-        print(input_mask.size())  # tensor:  b * max_length
-        print(segment_ids.size())  # tensor:  b * max_length
-        print(label_ids.size())  # tensor: b
+        print(batch)
 
-        if idx > 1:
-            break
+        # print(input_ids.size())  # tensor: b * max_length
+        # print(input_mask.size())  # tensor:  b * max_length
+        # print(segment_ids.size())  # tensor:  b * max_length
+        # print(label_ids.size())  # tensor: b
+
+
+        tmp_ids=torch.tensor([[0 for x in range(10)]]).to(device)
+        # loss, logits = model(input_ids, attention_mask=input_mask,token_type_ids=segment_ids,labels=label_ids) # Albert
+        loss, logits = model(input_ids, attention_mask=input_mask,labels=label_ids) # Roberta
+
+
+        print(loss)
+
+        break
+
+
+
+"""
+CUDA_LAUNCH_BLOCKING=1 ipython dataset_0413.py
+
+"""
