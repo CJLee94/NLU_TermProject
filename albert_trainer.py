fd93cae6fabf5f54ee91d77cddee55524d3e68fd
Felicia <shuyazhao2016@gmail.com>
2021-04-18 13:06:03 -0400

    Train with SGD-Nesterov momentum

--- albert_trainer.py
+++ albert_trainer.py
@@ -35,20 +35,30 @@ def albert_trainer(dataset_type="mnli"):
     # load the model
     model = AlbertForSequenceClassification.from_pretrained("albert-base-v2", num_labels=num_labels)
 
+    # ckpt_path="/media/felicia/Data/albert-{}-train/checkpoint-15000/".format(dataset_type)
+    # model = AlbertForSequenceClassification.from_pretrained(ckpt_path, num_labels=num_labels)
+
     # set all the training parameter
     batch_size = 32
     args = TrainingArguments(
         "albert-{}-train".format(dataset_type),
         evaluation_strategy="epoch",
-        learning_rate=2e-5,
+        # learning_rate=2e-5,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         num_train_epochs=5,
-        weight_decay=0.01,
+        # weight_decay=0.01,
         save_steps=5000,
         save_total_limit=10,
         load_best_model_at_end=True,
         metric_for_best_model='accuracy',
+    )
+
+    opt=torch.optim.SGD(
+        model.parameters(),
+        lr=0.1,
+        weight_decay=1e-4,
+        nesterov=True
     )
 
     # define a metric function
@@ -66,11 +76,12 @@ def albert_trainer(dataset_type="mnli"):
         train_dataset=encoded_dataset["train"],
         eval_dataset=encoded_dataset[validation_key],
         tokenizer=tokenizer,
-        compute_metrics=compute_metrics
+        compute_metrics=compute_metrics,
+        optimizers=opt
     )
 
     # train
-    trainer.train()
+    # trainer.train()
 
     # evaluate
     result = trainer.evaluate()
@@ -86,3 +97,20 @@ if __name__ == "__main__":
     """
 
     albert_trainer(dataset_type="qnli")
+
+""""
+albert-base-v2:
+{'eval_loss': 0.7939354777336121, 'eval_accuracy': 0.5053999633900788, 
+'eval_runtime': 29.1222, 'eval_samples_per_second': 187.589, 
+'init_mem_cpu_alloc_delta': 1843462144, 'init_mem_gpu_alloc_delta': 46745088, '
+init_mem_cpu_peaked_delta': 33431552, 'init_mem_gpu_peaked_delta': 0, 'eval_mem_cpu_alloc_delta': 18046976, 
+'eval_mem_gpu_alloc_delta': 0, 'eval_mem_cpu_peaked_delta': 49152, 'eval_mem_gpu_peaked_delta': 57188864}
+
+ckpt-15000:
+{'eval_loss': 0.49163737893104553, 'eval_accuracy': 0.9106717920556471, 
+'eval_runtime': 28.0782, 'eval_samples_per_second': 194.564, 'init_mem_cpu_alloc_delta': 1845587968, 
+'init_mem_gpu_alloc_delta': 46745088, 'init_mem_cpu_peaked_delta': 51974144, 
+'init_mem_gpu_peaked_delta': 0, 'eval_mem_cpu_alloc_delta': 19054592,
+ 'eval_mem_gpu_alloc_delta': 0, 'eval_mem_cpu_peaked_delta': 86016,
+  'eval_mem_gpu_peaked_delta': 57188864}
+"""
